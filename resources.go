da27cd26db93a675f334e24dc22398ed31941e1c
Ilya Brin <464157+ilyabrin@users.noreply.github.com>
2020-11-05 22:52:27 +0300

    GetLastUploadedResources added - wip

--- resources.go
+++ resources.go
@@ -243,10 +243,47 @@ func (c *Client) GetSortedFiles(ctx context.Context) (*FilesResourceList, *Error
 		return nil, nil
 	}
 	return files, nil
-}                                                                     // get | sortBy = [name = default, uploadDate]
-func (c *Client) MoveResource(ctx context.Context, path string)       {} // post
+}
+
+// get | sortBy = [name = default, uploadDate]
+func (c *Client) GetLastUploadedResources(ctx context.Context) (*LastUploadedResourceList, *ErrorResponse) {
+
+	var files *LastUploadedResourceList
+	var errorResponse *ErrorResponse
+	var err error
+	var decoded *json.Decoder
+
+	resp, err := c.doRequest(ctx, GET, "disk/resources/last-uploaded", nil)
+	if haveError(err) {
+		log.Fatal("Request failed")
+	}
+
+	if resp.StatusCode != 200 {
+		decoded = json.NewDecoder(resp.Body)
+		err := decoded.Decode(&errorResponse)
+		if haveError(err) {
+			log.Fatal(err)
+		}
+		return nil, errorResponse
+	}
+
+	decoded = json.NewDecoder(resp.Body)
+	if err := decoded.Decode(&files); err != nil {
+		log.Fatal(err)
+		return nil, nil
+	}
+
+	return files, nil
+}
+
+func (c *Client) MoveResource(ctx context.Context, path string) {} // post
+
 func (c *Client) GetPublicResources(ctx context.Context, path string) {} // get
-func (c *Client) PublishResource(ctx context.Context, path string)    {} // put
-func (c *Client) UnpublishResource(ctx context.Context, path string)  {} // put
-func (c *Client) GetLinkForUpload(ctx context.Context, path string)   {} // get
-func (c *Client) UploadFile(ctx context.Context, url string)          {} // post
+
+func (c *Client) PublishResource(ctx context.Context, path string) {} // put
+
+func (c *Client) UnpublishResource(ctx context.Context, path string) {} // put
+
+func (c *Client) GetLinkForUpload(ctx context.Context, path string) {} // get
+
+func (c *Client) UploadFile(ctx context.Context, url string) {} // post
