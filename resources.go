53854512a90bca2a48dd0aa0ccf4813848d9425d
Ilya Brin <464157+ilyabrin@users.noreply.github.com>
2020-11-05 22:46:10 +0300

    GetSortedFiles added - wip

--- resources.go
+++ resources.go
@@ -216,10 +216,37 @@ func (c *Client) GetDownloadURL(ctx context.Context, path string) (*Link, *Error
 	return link, nil
 }
 
-func (c *Client) GetSortedFiles(ctx context.Context, path string, sortBy string) {} // get | sortBy = [name = default, uploadDate]
-func (c *Client) MoveResource(ctx context.Context, path string)                  {} // post
-func (c *Client) GetPublicResources(ctx context.Context, path string)            {} // get
-func (c *Client) PublishResource(ctx context.Context, path string)               {} // put
-func (c *Client) UnpublishResource(ctx context.Context, path string)             {} // put
-func (c *Client) GetLinkForUpload(ctx context.Context, path string)              {} // get
-func (c *Client) UploadFile(ctx context.Context, url string)                     {} // post
+func (c *Client) GetSortedFiles(ctx context.Context) (*FilesResourceList, *ErrorResponse) {
+
+	var files *FilesResourceList
+	var errorResponse *ErrorResponse
+	var err error
+	var decoded *json.Decoder
+
+	resp, err := c.doRequest(ctx, GET, "disk/resources/files", nil)
+	if haveError(err) {
+		log.Fatal("Request failed")
+	}
+
+	if resp.StatusCode != 200 {
+		decoded = json.NewDecoder(resp.Body)
+		err := decoded.Decode(&errorResponse)
+		if haveError(err) {
+			log.Fatal(err)
+		}
+		return nil, errorResponse
+	}
+
+	decoded = json.NewDecoder(resp.Body)
+	if err := decoded.Decode(&files); err != nil {
+		log.Fatal(err)
+		return nil, nil
+	}
+	return files, nil
+}                                                                     // get | sortBy = [name = default, uploadDate]
+func (c *Client) MoveResource(ctx context.Context, path string)       {} // post
+func (c *Client) GetPublicResources(ctx context.Context, path string) {} // get
+func (c *Client) PublishResource(ctx context.Context, path string)    {} // put
+func (c *Client) UnpublishResource(ctx context.Context, path string)  {} // put
+func (c *Client) GetLinkForUpload(ctx context.Context, path string)   {} // get
+func (c *Client) UploadFile(ctx context.Context, url string)          {} // post
