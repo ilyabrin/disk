a93da64300deff98df42cf760f8004641ea0908a
Ilya Brin <464157+ilyabrin@users.noreply.github.com>
2020-11-06 16:31:08 +0300

    GetPublicResources added - wip

--- resources.go
+++ resources.go
@@ -305,7 +305,33 @@ func (c *Client) MoveResource(ctx context.Context, from, path string) (*Link, *E
 	return link, nil
 }
 
-func (c *Client) GetPublicResources(ctx context.Context, path string) {} // get
+func (c *Client) GetPublicResources(ctx context.Context) (*PublicResourcesList, *ErrorResponse) {
+	var list *PublicResourcesList
+	var errorResponse *ErrorResponse
+	var err error
+	var decoded *json.Decoder
+
+	resp, err := c.doRequest(ctx, GET, "disk/resources/public", nil)
+	if haveError(err) {
+		log.Fatal("Request failed")
+	}
+
+	if resp.StatusCode != 200 {
+		decoded = json.NewDecoder(resp.Body)
+		err := decoded.Decode(&errorResponse)
+		if haveError(err) {
+			log.Fatal(err)
+		}
+		return nil, errorResponse
+	}
+
+	decoded = json.NewDecoder(resp.Body)
+	if err := decoded.Decode(&list); err != nil {
+		log.Fatal(err)
+	}
+
+	return list, nil
+}
 
 func (c *Client) PublishResource(ctx context.Context, path string) {} // put
 
