309e8843e400d055d547d168c9ed00e17af18019
Ilya Brin <464157+ilyabrin@users.noreply.github.com>
2020-11-08 14:01:47 +0300

    GetDownloadURLForPublicResource added - wip

--- public.go
+++ public.go
@@ -34,6 +34,32 @@ func (c *Client) GetMetadataForPublicResource(ctx context.Context, public_key st
 	return resource, nil
 }
 
-func (c *Client) GetDownloadURLForPublicResource(ctx context.Context, public_key string) {}
+func (c *Client) GetDownloadURLForPublicResource(ctx context.Context, public_key string) (*Link, *ErrorResponse) {
+	var link *Link
+	var errorResponse *ErrorResponse
+	var err error
+	var decoded *json.Decoder
+
+	resp, err := c.doRequest(ctx, GET, "disk/public/resources/download?public_key="+public_key, nil)
+	if haveError(err) {
+		log.Fatal("Request failed")
+	}
+
+	if resp.StatusCode != 200 {
+		decoded = json.NewDecoder(resp.Body)
+		err := decoded.Decode(&errorResponse)
+		if haveError(err) {
+			log.Fatal(err)
+		}
+		return nil, errorResponse
+	}
+
+	decoded = json.NewDecoder(resp.Body)
+	if err := decoded.Decode(&link); err != nil {
+		log.Fatal(err)
+	}
+
+	return link, nil
+}
 
 func (c *Client) SavePublicResource(ctx context.Context, public_key string) {}
