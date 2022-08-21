8db698bc254ecf2138d2c7fe20b104ee57750f19
Ilya Brin <464157+ilyabrin@users.noreply.github.com>
2022-08-21 18:24:17 +0300

    wip: trash methods stubs

--- trash.go
+++ trash.go
@@ -4,10 +4,14 @@ import (
 	"context"
 )
 
-func (c *Client) DeleteFromTrash(ctx context.Context, path string) {}
+func (c *Client) DeleteFromTrash(ctx context.Context, path string) {
+	// TODO
+}
 
 func (c *Client) DeleteAllFromTrash(ctx context.Context) {
 	c.DeleteFromTrash(ctx, "trash root here")
 }
 
-func (c *Client) RestoreFromTrash(ctx context.Context, path string) {}
+func (c *Client) RestoreFromTrash(ctx context.Context, path string) {
+	// TODO
+}
