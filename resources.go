36061e2306db02b45a879cba10372637490a9421
Ilya Brin <464157+ilyabrin@users.noreply.github.com>
2022-11-02 19:01:48 +0300

    refactored: resources.go

--- resources.go
+++ resources.go
@@ -5,11 +5,8 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"log"
 	"os"
 )
-
-// todo: add *ErrorResponse to return
 
 func (c *Client) DeleteResource(ctx context.Context, path string, permanent bool, params *queryParams) *ErrorResponse {
 
@@ -45,11 +42,11 @@ func (c *Client) GetMetadata(ctx context.Context, path string, params *queryPara
 /*
 todo: add examples to README
 
-	newMeta := map[string]map[string]string{
+	newMeta := &metadata{
 		"custom_properties": {
-			"key_01": "value_01",
-			"key_02": "value_02",
-			"key_07": "value_07",
+			"key": "value",
+			"foo": "bar",
+			"platform": "linux",
 		},
 	}
 */
@@ -82,118 +79,63 @@ func (c *Client) CreateDir(ctx context.Context, path string, params *queryParams
 	var link *Link
 
 	resp, err := c.put(ctx, c.api_url+"resources?path="+path, nil, nil, params)
+
 	if haveError(err) || resp.StatusCode != 201 {
 		return nil, handleResponseCode(resp.StatusCode)
 	}
 
-	// if  {
-	// 	decoded = json.NewDecoder(resp.Body)
-	// 	err := decoded.Decode(&errorResponse)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 		return nil, nil
-	// 	}
-	// 	return nil, errorResponse
-	// }
-
-	err = json.NewDecoder(resp.Body).Decode(&link)
-	if err != nil {
-		log.Fatal(err)
-		return nil, nil
+	if err := json.NewDecoder(resp.Body).Decode(&link); err != nil {
+		return nil, jsonDecodeError(err)
 	}
+
 	return link, nil
 }
 
-func (c *Client) CopyResource(ctx context.Context, from, path string, params *queryParams) (*Link, *ErrorResponse) {
-	if len(from) < 1 || len(path) < 1 {
-		return nil, nil
-	}
+func (c *Client) CopyResource(ctx context.Context, from, to string, params *queryParams) (*Link, *ErrorResponse) {
 
 	var link *Link
-	var errorResponse *ErrorResponse
-	var err error
-	var decoded *json.Decoder
 
-	resp, err := c.doRequest(ctx, "POST", c.api_url+"resources/copy?from="+from+"&path="+path, nil, nil, params)
-	if haveError(err) {
-		log.Fatal("Request failed")
+	resp, err := c.post(ctx, c.api_url+"resources/copy?from="+from+"&path="+to, nil, nil, params)
+	if haveError(err) || !inArray(resp.StatusCode, []int{200, 201, 202}) {
+		return nil, handleResponseCode(resp.StatusCode)
 	}
 
-	if !inArray(resp.StatusCode, []int{200, 201, 202}) {
-		decoded = json.NewDecoder(resp.Body)
-		err := decoded.Decode(&errorResponse)
-		if err != nil {
-			log.Fatal(err)
-		}
-		return nil, errorResponse
+	if err := json.NewDecoder(resp.Body).Decode(&link); err != nil {
+		return nil, jsonDecodeError(err)
 	}
 
-	decoded = json.NewDecoder(resp.Body)
-	if err := decoded.Decode(&link); err != nil {
-		log.Fatal(err)
-		return nil, nil
-	}
 	return link, nil
 }
 
 func (c *Client) GetDownloadURL(ctx context.Context, path string, params *queryParams) (*Link, *ErrorResponse) {
-	if len(path) < 1 {
-		return nil, nil
-	}
 
 	var link *Link
-	var errorResponse *ErrorResponse
-	var err error
-	var decoded *json.Decoder
 
-	resp, err := c.doRequest(ctx, "GET", c.api_url+"resources/download?path="+path, nil, nil, params)
-	if haveError(err) {
-		log.Fatal("Request failed")
+	resp, err := c.get(ctx, c.api_url+"resources/download?path="+path, nil, params)
+	if haveError(err) || resp.StatusCode != 200 {
+		return nil, handleResponseCode(resp.StatusCode)
 	}
 
-	if resp.StatusCode != 200 {
-		decoded = json.NewDecoder(resp.Body)
-		err := decoded.Decode(&errorResponse)
-		if haveError(err) {
-			log.Fatal(err)
-		}
-		return nil, errorResponse
+	if err := json.NewDecoder(resp.Body).Decode(&link); err != nil {
+		return nil, jsonDecodeError(err)
 	}
 
-	decoded = json.NewDecoder(resp.Body)
-	if err := decoded.Decode(&link); err != nil {
-		log.Fatal(err)
-		return nil, nil
-	}
 	return link, nil
 }
 
 func (c *Client) GetSortedFiles(ctx context.Context, params *queryParams) (*FilesResourceList, *ErrorResponse) {
 
 	var files *FilesResourceList
-	var errorResponse *ErrorResponse
-	var err error
-	var decoded *json.Decoder
 
-	resp, err := c.doRequest(ctx, "GET", c.api_url+"resources/files", nil, nil, params)
-	if haveError(err) {
-		log.Fatal("Request failed")
+	resp, err := c.get(ctx, c.api_url+"resources/files", nil, params)
+	if haveError(err) || resp.StatusCode != 200 {
+		return nil, handleResponseCode(resp.StatusCode)
 	}
 
-	if resp.StatusCode != 200 {
-		decoded = json.NewDecoder(resp.Body)
-		err := decoded.Decode(&errorResponse)
-		if haveError(err) {
-			log.Fatal(err)
-		}
-		return nil, errorResponse
+	if err := json.NewDecoder(resp.Body).Decode(&files); err != nil {
+		return nil, jsonDecodeError(err)
 	}
 
-	decoded = json.NewDecoder(resp.Body)
-	if err := decoded.Decode(&files); err != nil {
-		log.Fatal(err)
-		return nil, nil
-	}
 	return files, nil
 }
 
@@ -201,169 +143,94 @@ func (c *Client) GetSortedFiles(ctx context.Context, params *queryParams) (*File
 func (c *Client) GetLastUploadedResources(ctx context.Context, params *queryParams) (*LastUploadedResourceList, *ErrorResponse) {
 
 	var files *LastUploadedResourceList
-	var errorResponse *ErrorResponse
-	var err error
-	var decoded *json.Decoder
 
-	resp, err := c.doRequest(ctx, "GET", c.api_url+"resources/last-uploaded", nil, nil, params)
-	if haveError(err) {
-		log.Fatal("Request failed")
+	resp, err := c.get(ctx, c.api_url+"resources/last-uploaded", nil, params)
+	if haveError(err) || resp.StatusCode != 200 {
+		return nil, handleResponseCode(resp.StatusCode)
 	}
 
-	if resp.StatusCode != 200 {
-		decoded = json.NewDecoder(resp.Body)
-		err := decoded.Decode(&errorResponse)
-		if haveError(err) {
-			log.Fatal(err)
-		}
-		return nil, errorResponse
-	}
-
-	decoded = json.NewDecoder(resp.Body)
-	if err := decoded.Decode(&files); err != nil {
-		log.Fatal(err)
-		return nil, nil
+	if err := json.NewDecoder(resp.Body).Decode(&files); err != nil {
+		return nil, jsonDecodeError(err)
 	}
 
 	return files, nil
 }
 
-func (c *Client) MoveResource(ctx context.Context, from, path string, params *queryParams) (*Link, *ErrorResponse) {
+func (c *Client) MoveResource(ctx context.Context, from, to string, params *queryParams) (*Link, *ErrorResponse) {
 
 	var link *Link
-	var errorResponse *ErrorResponse
-	var err error
-	var decoded *json.Decoder
 
-	resp, err := c.doRequest(ctx, "POST", c.api_url+"resources/move?from="+from+"&path="+path, nil, nil, params)
-	if haveError(err) {
-		log.Fatal("Request failed")
+	resp, err := c.post(ctx, c.api_url+"resources/move?from="+from+"&path="+to, nil, nil, params)
+	if haveError(err) || !inArray(resp.StatusCode, []int{201, 202}) {
+
 	}
 
-	if !inArray(resp.StatusCode, []int{201, 202}) {
-		decoded = json.NewDecoder(resp.Body)
-		err := decoded.Decode(&errorResponse)
-		if haveError(err) {
-			log.Fatal(err)
-		}
-		return nil, errorResponse
-	}
-
-	decoded = json.NewDecoder(resp.Body)
-	if err := decoded.Decode(&link); err != nil {
-		log.Fatal(err)
+	if err := json.NewDecoder(resp.Body).Decode(&link); err != nil {
+		return nil, jsonDecodeError(err)
 	}
 
 	return link, nil
 }
 
 func (c *Client) GetPublicResources(ctx context.Context, params *queryParams) (*PublicResourcesList, *ErrorResponse) {
+
 	var list *PublicResourcesList
-	var errorResponse *ErrorResponse
-	var err error
-	var decoded *json.Decoder
 
-	resp, err := c.doRequest(ctx, "GET", c.api_url+"resources/public", nil, nil, params)
-	if haveError(err) {
-		log.Fatal("Request failed")
+	resp, err := c.get(ctx, c.api_url+"resources/public", nil, params)
+	if haveError(err) || resp.StatusCode != 200 {
+		return nil, handleResponseCode(resp.StatusCode)
 	}
 
-	if resp.StatusCode != 200 {
-		decoded = json.NewDecoder(resp.Body)
-		err := decoded.Decode(&errorResponse)
-		if haveError(err) {
-			log.Fatal(err)
-		}
-		return nil, errorResponse
-	}
-
-	decoded = json.NewDecoder(resp.Body)
-	if err := decoded.Decode(&list); err != nil {
-		log.Fatal(err)
+	if err := json.NewDecoder(resp.Body).Decode(&list); err != nil {
+		return nil, jsonDecodeError(err)
 	}
 
 	return list, nil
 }
 
 func (c *Client) PublishResource(ctx context.Context, path string, params *queryParams) (*Link, *ErrorResponse) {
+
 	var link *Link
-	var errorResponse *ErrorResponse
-	var err error
-	var decoded *json.Decoder
 
-	resp, err := c.doRequest(ctx, "PUT", c.api_url+"resources/publish?path="+path, nil, nil, params)
-	if haveError(err) {
-		log.Fatal("Request failed")
+	resp, err := c.put(ctx, c.api_url+"resources/publish?path="+path, nil, nil, params)
+	if haveError(err) || resp.StatusCode != 200 {
+		return nil, handleResponseCode(resp.StatusCode)
 	}
 
-	if resp.StatusCode != 200 {
-		decoded = json.NewDecoder(resp.Body)
-		err := decoded.Decode(&errorResponse)
-		if haveError(err) {
-			log.Fatal(err)
-		}
-		return nil, errorResponse
-	}
-
-	decoded = json.NewDecoder(resp.Body)
-	if err := decoded.Decode(&link); err != nil {
-		log.Fatal(err)
+	if err := json.NewDecoder(resp.Body).Decode(&link); err != nil {
+		return nil, jsonDecodeError(err)
 	}
 
 	return link, nil
 }
 
 func (c *Client) UnpublishResource(ctx context.Context, path string, params *queryParams) (*Link, *ErrorResponse) {
+
 	var link *Link
-	var errorResponse *ErrorResponse
-	var err error
-	var decoded *json.Decoder
 
-	resp, err := c.doRequest(ctx, "PUT", c.api_url+"resources/unpublish?path="+path, nil, nil, params)
-	if haveError(err) {
-		log.Fatal("Request failed")
+	resp, err := c.put(ctx, c.api_url+"resources/unpublish?path="+path, nil, nil, params)
+	if haveError(err) || resp.StatusCode != 200 {
+		return nil, handleResponseCode(resp.StatusCode)
 	}
 
-	if resp.StatusCode != 200 {
-		decoded = json.NewDecoder(resp.Body)
-		err := decoded.Decode(&errorResponse)
-		if haveError(err) {
-			log.Fatal(err)
-		}
-		return nil, errorResponse
-	}
-
-	decoded = json.NewDecoder(resp.Body)
-	if err := decoded.Decode(&link); err != nil {
-		log.Fatal(err)
+	if err := json.NewDecoder(resp.Body).Decode(&link); err != nil {
+		return nil, jsonDecodeError(err)
 	}
 
 	return link, nil
 }
 
 func (c *Client) GetLinkForUpload(ctx context.Context, path string) (*Link, *ErrorResponse) {
+
 	var resource *Link
-	var errorResponse *ErrorResponse
-	var err error
-	var decoded *json.Decoder
 
-	resp, err := c.doRequest(ctx, "GET", c.api_url+"resources/upload?path="+path, nil, nil, nil)
-	if haveError(err) {
-		log.Fatal("Request failed")
+	resp, err := c.get(ctx, c.api_url+"resources/upload?path="+path, nil, nil)
+	if haveError(err) || resp.StatusCode != 200 {
+		return nil, handleResponseCode(resp.StatusCode)
 	}
 
-	if resp.StatusCode != 200 {
-		decoded = json.NewDecoder(resp.Body)
-		err := decoded.Decode(&errorResponse)
-		if haveError(err) {
-			log.Fatal(err)
-		}
-		return nil, errorResponse
-	}
-
-	decoded = json.NewDecoder(resp.Body)
-	if err := decoded.Decode(&resource); err != nil {
-		log.Fatal(err)
+	if err := json.NewDecoder(resp.Body).Decode(&resource); err != nil {
+		return nil, jsonDecodeError(err)
 	}
 
 	return resource, nil
@@ -371,21 +238,19 @@ func (c *Client) GetLinkForUpload(ctx context.Context, path string) (*Link, *Err
 
 func (c *Client) UploadFile(ctx context.Context, file_path, url string, params *queryParams) *ErrorResponse {
 
-	var decoded *json.Decoder
 	var errorResponse *ErrorResponse
 
 	f, err := os.Open(file_path)
 	if err != nil {
-		log.Fatal(err)
+		return jsonDecodeError(err)
 	}
 	body := bufio.NewReader(f)
 	defer f.Close()
 
-	resp, err := c.doRequest(ctx, "PUT", url, body, nil, nil)
+	resp, err := c.put(ctx, url, body, nil, nil)
 	if haveError(err) {
-		decoded = json.NewDecoder(resp.Body)
-		if err := decoded.Decode(&errorResponse); err != nil {
-			log.Fatal(err)
+		if err := json.NewDecoder(resp.Body).Decode(&errorResponse); err != nil {
+			return jsonDecodeError(err)
 		}
 	}
 
