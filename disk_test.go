8843ad0bd471abaf55d06e07ec1625c053265751
Ilya Brin <464157+ilyabrin@users.noreply.github.com>
2024-10-02 15:52:00 +0300

    wip: disk test added

--- disk_test.go
+++ disk_test.go
@@ -6,18 +6,15 @@ import (
 	"net"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-const TEST_DATA_DIR = "testdata/responses/"
-
 func testingHTTPClient(handler http.Handler) (*http.Client, func()) {
 	s := httptest.NewTLSServer(handler)
 
-	cli := &http.Client{
+	client := &http.Client{
 		Transport: &http.Transport{
 			DialContext: func(_ context.Context, network, _ string) (net.Conn, error) {
 				return net.Dial(network, s.Listener.Addr().String())
@@ -28,12 +25,7 @@ func testingHTTPClient(handler http.Handler) (*http.Client, func()) {
 		},
 	}
 
-	return cli, s.Close
-}
-
-func loadTestResponse(actionName string) []byte {
-	response, _ := os.ReadFile(TEST_DATA_DIR + actionName + ".json")
-	return response
+	return client, s.Close
 }
 
 func TestClientGetDiskInfo(t *testing.T) {
@@ -41,7 +33,35 @@ func TestClientGetDiskInfo(t *testing.T) {
 	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		assert.NotEmpty(t, r.Header.Get("Authorization"))
 		assert.Equal(t, "OAuth token", r.Header.Get("Authorization"))
-		w.Write(loadTestResponse("GET_disk"))
+
+		w.Write([]byte(`
+		{
+			"unlimited_autoupload_enabled": false,
+			"max_file_size": 53687091200,
+			"total_space": 1190242811904,
+			"is_paid": true,
+			"used_space": 664410431972,
+			"system_folders": {
+				"odnoklassniki": "disk:/Социальные сети/Одноклассники",
+				"google": "disk:/Социальные сети/Google+",
+				"instagram": "disk:/Социальные сети/Instagram",
+				"vkontakte": "disk:/Социальные сети/ВКонтакте",
+				"mailru": "disk:/Социальные сети/Мой Мир",
+				"downloads": "disk:/Загрузки/",
+				"applications": "disk:/Приложения",
+				"facebook": "disk:/Социальные сети/Facebook",
+				"social": "disk:/Социальные сети/",
+				"screenshots": "disk:/Скриншоты/",
+				"photostream": "disk:/Фотокамера/"
+			   },
+			"user": {
+			 "country": "ru",
+			 "login": "user",
+			 "display_name": "User Name",
+			 "uid": "12345678"
+			},
+			"revision": 1602851010832695
+		}`))
 	})
 
 	httpClient, teardown := testingHTTPClient(h)
@@ -52,6 +72,15 @@ func TestClientGetDiskInfo(t *testing.T) {
 
 	disk, err := client.DiskInfo(context.Background())
 
+	// check response data
 	assert.Nil(t, err)
 	assert.Equal(t, true, disk.IsPaid)
+	assert.Equal(t, 53687091200, disk.MaxFileSize)
+	assert.Equal(t, 1190242811904, disk.TotalSpace)
+	assert.Equal(t, "User Name", disk.User.DisplayName)
+
+	// check types
+	assert.IsType(t, Disk{}.IsPaid, disk.IsPaid)
+	assert.IsType(t, Disk{}.User, disk.User)
+	assert.IsType(t, Disk{}.SystemFolders, disk.SystemFolders)
 }
