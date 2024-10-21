e6eed95b8002c2b82628881d57882a1aadf5fc06
Ilya Brin <464157+ilyabrin@users.noreply.github.com>
2024-10-21 04:09:10 +0300

    trash and operations temporary removed

--- operations.go
+++ operations.go
@@ -1,24 +1,7 @@
 package disk
 
-import (
-	"context"
-	"encoding/json"
-)
+// TODO
 
-type OperationService service
-
-func (s *OperationService) Status(ctx context.Context, operationID string, params *QueryParams) (*Operation, *ErrorResponse) {
-	resp, err := s.client.get(ctx, s.client.apiURL+"operations/"+operationID, params)
-	if err != nil { // || resp.StatusCode != http.StatusOK {
-		return nil, handleResponseCode(resp.StatusCode)
-	}
-	defer resp.Body.Close()
-
-	operation := new(Operation)
-	err = json.NewDecoder(resp.Body).Decode(&operation)
-	if err != nil {
-		return nil, jsonDecodeError(err)
-	}
-
-	return operation, nil
-}
+// func (c *Client) Status(ctx context.Context, operationId string, params *QueryParams) (*Operation, *ErrorResponse) {
+// 	return nil, nil
+// }
