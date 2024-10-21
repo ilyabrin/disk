f94cb294fc5ce930a03c04440b217ee41d1f58c8
Ilya Brin <464157+ilyabrin@users.noreply.github.com>
2024-10-21 04:32:19 +0300

    add operations

--- operations.go
+++ operations.go
@@ -1,7 +1,32 @@
 package disk
 
-// TODO
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
-// func (c *Client) Status(ctx context.Context, operationId string, params *QueryParams) (*Operation, *ErrorResponse) {
-// 	return nil, nil
-// }
+// TODO: add tests and use generics instead of interface{}
+func (c *Client) OperationStatus(ctx context.Context, operationID string) (interface{}, *http.Response, error) {
+	resp, err := c.doRequest(ctx, GET, fmt.Sprintf("operations/operation_id=%s", operationID), nil)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to make request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		var errorResp ErrorResponse
+		if err := json.NewDecoder(resp.Body).Decode(&errorResp); err != nil {
+			return nil, resp, fmt.Errorf("failed to decode error response: %w", err)
+		}
+		return &errorResp, resp, nil
+	}
+
+	var operation Operation
+	if err := json.NewDecoder(resp.Body).Decode(&operation); err != nil {
+		return nil, resp, fmt.Errorf("failed to decode operation: %w", err)
+	}
+
+	return &operation, resp, nil
+}
